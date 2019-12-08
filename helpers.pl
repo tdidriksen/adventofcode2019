578a82dcabd08eba5872de5c50889682e3a51255
Thomas Didriksen <t.didriksen@gmail.com>
2019-12-08 17:07:06 +0100

    Add char_to_string and digit_list to helpers

--- helpers.pl
+++ helpers.pl
@@ -1,4 +1,4 @@
-:- module(helpers, [file_to_stringlist/3, file_to_numberlist/3]).
+:- module(helpers, [file_to_stringlist/3, file_to_numberlist/3, char_to_string/2, digit_list/2]).
 :- use_module(library(readutil)).
 
 file_to_stringlist(I, Sep, O) :-
@@ -8,3 +8,10 @@ file_to_stringlist(I, Sep, O) :-
 file_to_numberlist(I, Sep, O) :-
     file_to_stringlist(I, Sep, T),
     maplist(atom_number, T, O).
+
+char_to_string(C, S) :- string_chars(S, [C]).
+
+digit_list(I, L) :-
+    number_string(I, S),
+    string_chars(S, Cs),
+    maplist(char_to_string, Cs, L).
